a0203d68dbade56784fe76c06e534375afa3545a
binzhulab <51965629+binzhulab@users.noreply.github.com>
2024-07-04 00:12:56 +0900

    Add files via upload

--- Generating_L/L_matrix_Generation.R
+++ Generating_L/L_matrix_Generation.R
@@ -15,22 +15,34 @@ Panel_2 <- read.table(file.path(path, "Panel_Info_2_assays.txt"), header = T, qu
 ## End_Position: end position of targeted panel
 ## SEQ_ASSAY_ID: distinguish different panels
 ## The unit of the returned L matrix is the number of trinucleotides per million base pairs
-L_matrix_generation <- function(genomic_information){
+L_matrix_generation <- function(genomic_information, Types = c("COSMIC", "signeR")){
   
   if(!all(c("Chromosome", "Start_Position", "End_Position", "SEQ_ASSAY_ID") %in% colnames(genomic_information))){
     stop("Please specify the column names of the argument genomic_information in (Chromosome, Start_Position, End_Position, SEQ_ASSAY_ID)")
   } 
+  if(length(Types) == 2){
+    stop("Please specifiy mutation types: avialable mutatioins types are either \"COSMIC\" or \"signeR\"")
+  }
   
   # mutation type categories
-  # Alexandrov v3.4 mutation type order
-  Alex_type <- c("A[C>A]A", "A[C>A]C", "A[C>A]G", "A[C>A]T", "A[C>G]A", "A[C>G]C", "A[C>G]G", "A[C>G]T", "A[C>T]A", "A[C>T]C", "A[C>T]G", "A[C>T]T", 
-                 "A[T>A]A", "A[T>A]C", "A[T>A]G", "A[T>A]T", "A[T>C]A", "A[T>C]C", "A[T>C]G", "A[T>C]T", "A[T>G]A", "A[T>G]C", "A[T>G]G", "A[T>G]T", 
-                 "C[C>A]A", "C[C>A]C", "C[C>A]G", "C[C>A]T", "C[C>G]A", "C[C>G]C", "C[C>G]G", "C[C>G]T", "C[C>T]A", "C[C>T]C", "C[C>T]G", "C[C>T]T", 
-                 "C[T>A]A", "C[T>A]C", "C[T>A]G", "C[T>A]T", "C[T>C]A", "C[T>C]C", "C[T>C]G", "C[T>C]T", "C[T>G]A", "C[T>G]C", "C[T>G]G", "C[T>G]T", 
-                 "G[C>A]A", "G[C>A]C", "G[C>A]G", "G[C>A]T", "G[C>G]A", "G[C>G]C", "G[C>G]G", "G[C>G]T", "G[C>T]A", "G[C>T]C", "G[C>T]G", "G[C>T]T", 
-                 "G[T>A]A", "G[T>A]C", "G[T>A]G", "G[T>A]T", "G[T>C]A", "G[T>C]C", "G[T>C]G", "G[T>C]T", "G[T>G]A", "G[T>G]C", "G[T>G]G", "G[T>G]T", 
-                 "T[C>A]A", "T[C>A]C", "T[C>A]G", "T[C>A]T", "T[C>G]A", "T[C>G]C", "T[C>G]G", "T[C>G]T", "T[C>T]A", "T[C>T]C", "T[C>T]G", "T[C>T]T", 
-                 "T[T>A]A", "T[T>A]C", "T[T>A]G", "T[T>A]T", "T[T>C]A", "T[T>C]C", "T[T>C]G", "T[T>C]T", "T[T>G]A", "T[T>G]C", "T[T>G]G", "T[T>G]T")
+  # Alexandrov v3.2 mutation type order
+  COSMIC <- c("A[C>A]A", "A[C>A]C", "A[C>A]G", "A[C>A]T", "C[C>A]A", "C[C>A]C", "C[C>A]G", "C[C>A]T", "G[C>A]A", "G[C>A]C", "G[C>A]G", "G[C>A]T", 
+              "T[C>A]A", "T[C>A]C", "T[C>A]G", "T[C>A]T", "A[C>G]A", "A[C>G]C", "A[C>G]G", "A[C>G]T", "C[C>G]A", "C[C>G]C", "C[C>G]G", "C[C>G]T", 
+              "G[C>G]A", "G[C>G]C", "G[C>G]G", "G[C>G]T", "T[C>G]A", "T[C>G]C", "T[C>G]G", "T[C>G]T", "A[C>T]A", "A[C>T]C", "A[C>T]G", "A[C>T]T", 
+              "C[C>T]A", "C[C>T]C", "C[C>T]G", "C[C>T]T", "G[C>T]A", "G[C>T]C", "G[C>T]G", "G[C>T]T", "T[C>T]A", "T[C>T]C", "T[C>T]G", "T[C>T]T", 
+              "A[T>A]A", "A[T>A]C", "A[T>A]G", "A[T>A]T", "C[T>A]A", "C[T>A]C", "C[T>A]G", "C[T>A]T", "G[T>A]A", "G[T>A]C", "G[T>A]G", "G[T>A]T", 
+              "T[T>A]A", "T[T>A]C", "T[T>A]G", "T[T>A]T", "A[T>C]A", "A[T>C]C", "A[T>C]G", "A[T>C]T", "C[T>C]A", "C[T>C]C", "C[T>C]G", "C[T>C]T", 
+              "G[T>C]A", "G[T>C]C", "G[T>C]G", "G[T>C]T", "T[T>C]A", "T[T>C]C", "T[T>C]G", "T[T>C]T", "A[T>G]A", "A[T>G]C", "A[T>G]G", "A[T>G]T", 
+              "C[T>G]A", "C[T>G]C", "C[T>G]G", "C[T>G]T", "G[T>G]A", "G[T>G]C", "G[T>G]G", "G[T>G]T", "T[T>G]A", "T[T>G]C", "T[T>G]G", "T[T>G]T")
+
+  signeR <- c("C>A:ACA", "C>A:ACC", "C>A:ACG", "C>A:ACT", "C>A:CCA", "C>A:CCC", "C>A:CCG", "C>A:CCT", "C>A:GCA", "C>A:GCC", "C>A:GCG", "C>A:GCT", 
+              "C>A:TCA", "C>A:TCC", "C>A:TCG", "C>A:TCT", "C>G:ACA", "C>G:ACC", "C>G:ACG", "C>G:ACT", "C>G:CCA", "C>G:CCC", "C>G:CCG", "C>G:CCT", 
+              "C>G:GCA", "C>G:GCC", "C>G:GCG", "C>G:GCT", "C>G:TCA", "C>G:TCC", "C>G:TCG", "C>G:TCT", "C>T:ACA", "C>T:ACC", "C>T:ACG", "C>T:ACT", 
+              "C>T:CCA", "C>T:CCC", "C>T:CCG", "C>T:CCT", "C>T:GCA", "C>T:GCC", "C>T:GCG", "C>T:GCT", "C>T:TCA", "C>T:TCC", "C>T:TCG", "C>T:TCT", 
+              "T>A:ATA", "T>A:ATC", "T>A:ATG", "T>A:ATT", "T>A:CTA", "T>A:CTC", "T>A:CTG", "T>A:CTT", "T>A:GTA", "T>A:GTC", "T>A:GTG", "T>A:GTT", 
+              "T>A:TTA", "T>A:TTC", "T>A:TTG", "T>A:TTT", "T>C:ATA", "T>C:ATC", "T>C:ATG", "T>C:ATT", "T>C:CTA", "T>C:CTC", "T>C:CTG", "T>C:CTT", 
+              "T>C:GTA", "T>C:GTC", "T>C:GTG", "T>C:GTT", "T>C:TTA", "T>C:TTC", "T>C:TTG", "T>C:TTT", "T>G:ATA", "T>G:ATC", "T>G:ATG", "T>G:ATT", 
+              "T>G:CTA", "T>G:CTC", "T>G:CTG", "T>G:CTT", "T>G:GTA", "T>G:GTC", "T>G:GTG", "T>G:GTT", "T>G:TTA", "T>G:TTC", "T>G:TTG", "T>G:TTT")
   
   # To handle L matrix
   # define trinucleotide seq from 5' to 3'. They are the same order as the function trinucleotideFrequency()
@@ -83,8 +95,14 @@ L_matrix_generation <- function(genomic_information){
     summarise_at(tri_nt_CT,sum)
   assay_CT_32$assaySize = rowSums(assay_CT_32[,-1])
   
-  assay_CT_32_2 <- data.frame(t(assay_CT_32[, paste0(substr(Alex_type, 1, 1), substr(Alex_type, 3, 3), substr(Alex_type, 7, 7)), drop = FALSE]))/10^6
-  rownames(assay_CT_32_2) <- Alex_type
+  if(Types == "COSMIC"){
+    assay_CT_32_2 <- data.frame(t(assay_CT_32[, paste0(substr(COSMIC, 1, 1), substr(COSMIC, 3, 3), substr(COSMIC, 7, 7)), drop = FALSE]))/10^6
+    rownames(assay_CT_32_2) <- COSMIC
+  } else if(Types == "signeR"){
+    assay_CT_32_2 <- data.frame(t(assay_CT_32[, substr(signeR, 5, 7), drop = FALSE]))/10^6
+    rownames(assay_CT_32_2) <- signeR  
+  } 
+  
   colnames(assay_CT_32_2) <- assay_CT_32$SEQ_ASSAY_ID
   
   return(assay_CT_32_2)
@@ -92,35 +110,30 @@ L_matrix_generation <- function(genomic_information){
 }
 
 
-L_matrix_generation(Panel_1)
+L_matrix_generation(Panel_1) #not working
+L_matrix_generation(Panel_1, Types = "COSMIC")
 ## Part of the results:
 #         UHN-48-V1
 # A[C>A]A  0.001487
 # A[C>A]C  0.001120
 # A[C>A]G  0.000426
 # A[C>A]T  0.001155
-# A[C>G]A  0.001487
-# A[C>G]C  0.001120
-# A[C>G]G  0.000426
-# A[C>G]T  0.001155
-# A[C>T]A  0.001487
-# A[C>T]C  0.001120
-# A[C>T]G  0.000426
+# C[C>A]A  0.001654
+# C[C>A]C  0.001300
+# C[C>A]G  0.000543
+# C[C>A]T  0.001464
 
-L_matrix_generation(Panel_2)
-## Part of the results:
-#         GRCC-CP1 UHN-48-V1
-# A[C>A]A 0.000883  0.001487
-# A[C>A]C 0.000656  0.001120
-# A[C>A]G 0.000278  0.000426
-# A[C>A]T 0.000687  0.001155
-# A[C>G]A 0.000883  0.001487
-# A[C>G]C 0.000656  0.001120
-# A[C>G]G 0.000278  0.000426
-# A[C>G]T 0.000687  0.001155
-# A[C>T]A 0.000883  0.001487
-# A[C>T]C 0.000656  0.001120
-# A[C>T]G 0.000278  0.000426
+L_matrix_generation(Panel_1, Types = "signeR")
+#         UHN-48-V1
+# C>A:ACA  0.001487
+# C>A:ACC  0.001120
+# C>A:ACG  0.000426
+# C>A:ACT  0.001155
+# C>A:CCA  0.001654
+# C>A:CCC  0.001300
+# C>A:CCG  0.000543
+# C>A:CCT  0.001464
 
-
-
+###########################################################
+L_matrix_generation(Panel_2, Types = "COSMIC")
+L_matrix_generation(Panel_2, Types = "signeR")

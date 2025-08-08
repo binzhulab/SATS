45fcdf222c2b80edf75d9ffa729501395e6491d1
wheelerb <33498685+wheelerb@users.noreply.github.com>
2025-08-08 08:55:20 -0400

    Add files via upload

--- nextflow/example1/bin/run_GeneratePanelSize.R
+++ nextflow/example1/bin/run_GeneratePanelSize.R
@@ -21,9 +21,11 @@ tmp <- try(load(infile), silent=FALSE)
 if ("try-error" %in% class(tmp)) {
   stop("ERROR: input file (arg[1]) must be an .rda file to be loaded with the load() function")
 }
-obj <- c("genomic_information", "Types")
+obj <- c("genomic_information", "Class", "SBS_order", "ref.genome")
 if (!(obj[1] %in% tmp)) stop("ERROR: input file must contain the object 'genomic_information'")
-if (!(obj[2] %in% tmp)) stop("ERROR: input file must contain the object 'Types'")
+if (!(obj[2] %in% tmp)) Class      <- "SBS"
+if (!(obj[3] %in% tmp)) SVS_order  <- "COSMIC"
+if (!(obj[4] %in% tmp)) ref.genome <- "hg19"
 
 # Install devtools package 
 if (!require("devtools")) install.packages("devtools", repos="https://cloud.r-project.org")
@@ -34,5 +36,9 @@ devtools::install_github("binzhulab/SATS", subdir="source", upgrade="never", lib
 # Load SATS and run the function
 library(SATS)
 
-ret <- SATS::GeneratePanelSize(genomic_information, Types)
+ret <- SATS::GeneratePanelSize(genomic_information, Class=Class, SBS_order=SBS_order,
+                               ref.genome=ref.genome)
 save(ret, file=outfile)
+
+
+
